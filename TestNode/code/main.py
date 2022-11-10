9865c6b880b7d69d8f7bf63cb014b6d4f5ddc5b0
NdA994 <nda994@gmail.com>
2022-11-10 15:24:18 -0800

    complete test script, ref #2

--- TestNode/code/main.py
+++ TestNode/code/main.py
@@ -3,7 +3,7 @@ import argparse
 from pymodbus.client import ModbusTcpClient  # pylint: disable-msg=E0611
 
 
-def cb_test(cb_index, value):
+def cb_write_test(cb_index, value):
     """test cd"""
     if cb_index == 0:
         client = ModbusTcpClient('10.0.0.24', 502)
@@ -39,6 +39,36 @@ def cb_test(cb_index, value):
         print('qualcosa')
 
 
+def cb_read_test(cb_index):
+    """read test"""
+    if cb_index == 0:
+        client = ModbusTcpClient('10.0.0.24', 502)
+        result = client.read_coils(0, 1)
+        print("Generation PLC")
+        print("CB00: " + str(result.bits[0]))
+    elif cb_index in (3, 7, 8):
+        client = ModbusTcpClient('10.0.0.22', 502)
+        result = client.read_coils(0, 3)
+        print("Trasmission PLC")
+        print("CB03: " + str(result.bits[0]))
+        print("CB07: " + str(result.bits[1]))
+        print("CB08: " + str(result.bits[2]))
+    elif cb_index in (4, 5):
+        client = ModbusTcpClient('10.0.0.23', 502)
+        result = client.read_coils(0, 2)
+        print("Micro Grid PLC")
+        print("CB04: " + str(result.bits[0]))
+        print("CB05: " + str(result.bits[1]))
+    elif cb_index in (10, 11, 12, 13):
+        client = ModbusTcpClient('10.0.0.21', 502)
+        result = client.read_coils(0, 4)
+        print("Smart Home PLC")
+        print("CB09: " + str(result.bits[0]))
+        print("CB10: " + str(result.bits[0]))
+        print("CB11: " + str(result.bits[0]))
+        print("CB12: " + str(result.bits[0]))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Modbus Server to manage CB \
         status variable')
@@ -49,6 +79,15 @@ if __name__ == '__main__':
                         action='store_true')
     parser.add_argument('-f', '--false', dest='true', help='set False to CB',
                         action='store_false')
+    parser.add_argument('-w', '--write', help='set True to CB (default)',
+                        action='store_true')
+    parser.add_argument('-r', '--read', dest='write', help='set False to CB',
+                        action='store_false')
     parser.set_defaults(true=True)
+    parser.set_defaults(write=True)
     args = parser.parse_args()
-    cb_test(args.cb, args.true)
+
+    if args.write:
+        cb_write_test(args.cb, args.true)
+    else:
+        cb_read_test(args.cb)
