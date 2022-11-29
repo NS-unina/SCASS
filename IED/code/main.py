aa7756e1090cf8e29ac8c0c9defffe1274c2ab6c
NdA994 <nda994@gmail.com>
2022-11-28 23:51:49 -0800

    manage status and complete implementation Generic IED, ref #1

--- IED/code/main.py
+++ IED/code/main.py
@@ -2,12 +2,10 @@
 import time
 from pymodbus.client import ModbusTcpClient  # pylint: disable-msg=E0611
 
-
-CB_STATUS = 0
-CB_INDEX = "0"
-
+time.sleep(5)
 
 if __name__ == '__main__':
+    print("Run")
     while True:
         client = ModbusTcpClient('127.0.0.1', 502)
         result = client.read_coils(0, 1)
