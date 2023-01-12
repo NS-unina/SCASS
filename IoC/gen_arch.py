e012d050b41bad79f41e06be1a1dd40ce4ccc5ee
Luca Castiglione <l.castiglione18@imperial.ac.uk>
2023-01-13 00:12:54 +0100

    removed master files from this branch

--- IoC/gen_arch.py
+++
@@ -1,7 +0,0 @@
-"""architecture generator module"""
-import yaml
-
-dict_file = [{'smartgrid'}]
-
-with open('arch.yaml', "w", encoding="utf-8") as file:
-    documents = yaml.dump(dict_file, file)
--- setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ip link set eth0 promisc on
