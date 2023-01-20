71668d83feef41576886c6e7a80fd7bf01de4979
NdA994 <nda994@gmail.com>
2023-01-20 07:41:59 -0800

    add control logic and attack goal (WIP), ref #18

--- architecture-mulval/epic_new.pl
+++ architecture-mulval/epic_new.pl
@@ -1,26 +1,49 @@
+/* Attack goal */
+attackGoal(canTamper(controlAction12)).
+attackGoal(canSpoof(controlAction12)).
+attackGoal(canTamper(controlAction4)).
+attackGoal(canSpoof(controlAction4)).
+attackGoal(canTamper(controlAction2)).
+attackGoal(canSpoof(controlAction2)).
+
+/* Attacker Location */
+attackerLocated(intranet).
+
 /* Enable comunication between host in the same subnet*/
 hacl(X,Y,_,_):-
 	inSubnet(X,S),
 	inSubnet(Y,S).
 
-/*scada network*/
+/* Control Logic */
+/* Unsafe configuration of breakers */
+controlsFlow(sPLC, controlAction12).
+transportsFlow(shRouter, controlAction12).
+/* CB-7 is open while the power is supplied through P3 */
+controlsFlow(scadaWorkStation, controlAction4).
+transportsFlow(scadaRouter, controlAction4).
+/* The circuit breaker CB-12 is open*/
+controlsFlow(scadaWorkStation, controlAction2).
+transportsFlow(scadaRouter, controlAction2).
+
+/* Scada network*/
 inSubnet(scadaWorkStation, scadaNet).
 inSubnet(historian, scadaNet).
 inSubnet(engineeringWorkStation, scadaNet).
 inSubnet(scadaRouter, scadaNet).
+/* Enable comunication between scadaWorkStation and the attacker */
 hacl(scadaWorkStation, intranet, _, _).
-/*scada hacl rules*/
+/* Scada hacl rules*/
 hacl(scadaWorkStation, genPLC, _, _).
 hacl(scadaWorkStation, shPLC, _, _).
 hacl(scadaWorkStation, mgPLC, _, _).
 hacl(scadaWorkStation, trPLC, _, _).
 
-/*generation network*/
+/* Generation network*/
 inSubnet(gIED1, generationNet).
 inSubnet(genPLC, generationNet).
 inSubnet(gRouter, generationNet).
 
-/*smarthome network*/
+/* SmartHome network*/
 inSubnet(sIED1, smartHomeNet).
 inSubnet(sIED2, smartHomeNet).
 inSubnet(sIED3, smartHomeNet).
@@ -28,32 +51,32 @@ inSubnet(sIED4, smartHomeNet).
 inSubnet(shPLC, smartHomeNet).
 inSubnet(shRouter, smartHomeNet).
 
-/*microgrid network*/
+/* MicroGrid network*/
 inSubnet(mIED1, mgNet).
 inSubnet(mIED2, mgNet).
 inSubnet(mgPLC, mgNet).
 inSubnet(mgRouter, mgNet).
 
-/*trasmission network*/
+/* Trasmission network*/
 inSubnet(tIED1, mgNet).
 inSubnet(tIED2, mgNet).
 inSubnet(tIED3, mgNet).
 inSubnet(trPLC, mgNet).
 inSubnet(tRouter, mgNet).
 
-/*TO CHECK - router link*/
-/*generation microgrid lan*/
+/* TO CHECK - router link*/
+/* Generation MicroGrid lan*/
 inSubnet(gRouter, gmNet).
 inSubnet(mgRouter, gmNet).
-/*generation trasmission lan*/
+/* Generation Trasmission lan*/
 inSubnet(gRouter, gtNet).
 inSubnet(tRouter, gtNet).
-/*trasmission scada lan*/
+/* Trasmission Scada lan*/
 inSubnet(tRouter, tsNet).
 inSubnet(scadaRouter, tsNet).
-/*smarthome scada lan*/
+/* SmartHome Scada lan*/
 inSubnet(shRouter, ssNet).
 inSubnet(scadaRouter, ssNet).
-/*smarthome microgrid lan*/
+/* SmartHome Microgrid lan*/
 inSubnet(shRouter, smNet).
 inSubnet(mgRouter, smNet).
