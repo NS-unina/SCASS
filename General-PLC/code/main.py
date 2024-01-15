adc7021cfef4d0d0842a643d882ac3238bfa2560
Dooowens <domensiorio@gmail.com>
2024-01-15 16:57:34 +0100

    aggiunta gestione eccezioni

--- General-PLC/code/main.py
+++ General-PLC/code/main.py
@@ -15,65 +15,91 @@ if __name__ == '__main__':
         client = ModbusTcpClient('127.0.0.1', 502)
         result = client.read_coils(0, 10)
         
-        splc = ModbusTcpClient("192.168.72.45", 502)
-        splc.write_coil(0, result.bits[0])
-        splc.write_coil(1, result.bits[1])
-        splc.write_coil(2, result.bits[2])
-        splc.write_coil(3, result.bits[3])
-        splc.close()
+        try:
+            splc = ModbusTcpClient("192.168.72.45", 502)
+            splc.write_coil(0, result.bits[0])
+            splc.write_coil(1, result.bits[1])
+            splc.write_coil(2, result.bits[2])
+            splc.write_coil(3, result.bits[3])
+            splc.close()
+        except:
+            print("Connessione con splc fallita, riprovare")
         
-        tplc = ModbusTcpClient("192.168.72.61", 502)
-        tplc.write_coil(0, result.bits[4])
-        tplc.write_coil(1, result.bits[5])
-        tplc.write_coil(2, result.bits[6])
-        tplc.close()
-        
-        mplc = ModbusTcpClient("192.168.72.29", 502)
-        mplc.write_coil(0, result.bits[7])
-        mplc.write_coil(1, result.bits[8])
-        mplc.close()
+        try:
+            tplc = ModbusTcpClient("192.168.72.61", 502)
+            tplc.write_coil(0, result.bits[4])
+            tplc.write_coil(1, result.bits[5])
+            tplc.write_coil(2, result.bits[6])
+            tplc.close()
+        except:
+            print("Connessione con tplc fallita, riprovare")
 
-        gplc = ModbusTcpClient("192.168.72.82", 502)
-        gplc.write_coil(0, result.bits[9])        
-        gplc.close()
+        try:
+            mplc = ModbusTcpClient("192.168.72.29", 502)
+            mplc.write_coil(0, result.bits[7])
+            mplc.write_coil(1, result.bits[8])
+            mplc.close()
+        except:
+            print("Connessione con mplc fallita, riprovare")
+
+        try:
+            gplc = ModbusTcpClient("192.168.72.93", 502)
+            gplc.write_coil(0, result.bits[9])        
+            gplc.close()
+        except:
+            print("Connessione con gplc fallita, riprovare")
+
 
         time.sleep(1)
 
-        splc = ModbusTcpClient("192.168.72.45", 502)
-        readSPLC = splc.read_coils(8, 25)
-        client.write_coil(22, readSPLC.bits[0])
-        status["S-IED1"] = readSPLC.bits[0]
-        client.write_coil(23, readSPLC.bits[8])
-        status["S-IED2"] = readSPLC.bits[8]
-        client.write_coil(24, readSPLC.bits[16])
-        status["S-IED3"] = readSPLC.bits[16]
-        client.write_coil(25, readSPLC.bits[24])
-        status["S-IED4"] = readSPLC.bits[24]
-        splc.close()
-        
-        tplc = ModbusTcpClient("192.168.72.61", 502)
-        readTPLC = tplc.read_coils(8, 17)
-        client.write_coil(36, readTPLC.bits[0])
-        status["T-IED1"] = readTPLC.bits[0]
-        client.write_coil(37, readTPLC.bits[8])
-        status["T-IED2"] = readTPLC.bits[8]
-        client.write_coil(38, readTPLC.bits[16])
-        status["T-IED3"] = readTPLC.bits[16]
-        tplc.close()
-        
-        mplc = ModbusTcpClient("192.168.72.29", 502)
-        readMPLC = mplc.read_coils(8, 9)
-        client.write_coil(50, readMPLC.bits[0])
-        status["M-IED1"] = readMPLC.bits[0]
-        client.write_coil(51, readMPLC.bits[8])
-        status["M-IED2"] = readMPLC.bits[8]
-        mplc.close()
-        
-        gplc = ModbusTcpClient("192.168.72.93", 502)
-        readGPLC = gplc.read_coils(8, 1)
-        client.write_coil(56, readGPLC.bits[0])
-        status["G-IED1"] = readGPLC.bits[0]
-        gplc.close()
+        try:
+            splc = ModbusTcpClient("192.168.72.45", 502)
+            readSPLC = splc.read_coils(8, 25)
+            client.write_coil(22, readSPLC.bits[0])
+            status["S-IED1"] = readSPLC.bits[0]
+            client.write_coil(23, readSPLC.bits[8])
+            status["S-IED2"] = readSPLC.bits[8]
+            client.write_coil(24, readSPLC.bits[16])
+            status["S-IED3"] = readSPLC.bits[16]
+            client.write_coil(25, readSPLC.bits[24])
+            status["S-IED4"] = readSPLC.bits[24]
+            splc.close()
+        except:
+            print("Connessione con splc fallita, riprovare")
+            
+        try:
+            tplc = ModbusTcpClient("192.168.72.61", 502)
+            readTPLC = tplc.read_coils(8, 17)
+            client.write_coil(36, readTPLC.bits[0])
+            status["T-IED1"] = readTPLC.bits[0]
+            client.write_coil(37, readTPLC.bits[8])
+            status["T-IED2"] = readTPLC.bits[8]
+            client.write_coil(38, readTPLC.bits[16])
+            status["T-IED3"] = readTPLC.bits[16]
+            tplc.close()
+        except:
+            print("Connessione con tplc fallita, riprovare")
+   
+        try:
+            mplc = ModbusTcpClient("192.168.72.29", 502)
+            readMPLC = mplc.read_coils(8, 9)
+            client.write_coil(50, readMPLC.bits[0])
+            status["M-IED1"] = readMPLC.bits[0]
+            client.write_coil(51, readMPLC.bits[8])
+            status["M-IED2"] = readMPLC.bits[8]
+            mplc.close()
+        except:
+            print("Connessione con mplc fallita, riprovare")
+   
+        try:
+            gplc = ModbusTcpClient("192.168.72.93", 502)
+            readGPLC = gplc.read_coils(8, 1)
+            client.write_coil(56, readGPLC.bits[0])
+            status["G-IED1"] = readGPLC.bits[0]
+            gplc.close()
+        except:
+            print("Connessione con gplc fallita, riprovare")
+
         
         f = open("status.txt", "w", encoding="utf-8")
         for key, value in status.items():
